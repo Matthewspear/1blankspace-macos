ff4b13b2a2e2ad76d4f81912d0260a2e71000b89
Matthew Spear <Matthewtimothyspear@googlemail.com>
2016-08-03 23:16:24 +0100

    Setup Xcode project

---
+++ 1blankspace/1blankspace/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  1blankspace
+//
+//  Created by Matthew Spear on 03/08/2016.
+//  Copyright © 2016 Matthew Spear. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
